07b8c35d55ec4a76d8f7145fffafce0251f02b8d
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-05-02 15:18:33 +0000

    added a markdown file

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 //console.log(words);
+// var input = document.getElementById("input").value;
 
 function check(word) {
     if (words[word]) {
-        console.log("exists");
+        console.log("Exists");
     } else {
         console.log("Does Not Exist");
     }
     console.log(word);
+    // document.getElementById("results").innerHTML = input;
 }
 
 check("car", words);
 
 app.get("/", function (req, res) { 
-   const userName = req.query.user;
-    res.send("Hello " + userName + "!");
+    res.send(words);
     
 });
 
